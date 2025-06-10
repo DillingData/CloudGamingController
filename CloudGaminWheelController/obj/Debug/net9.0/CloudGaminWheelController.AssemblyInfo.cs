3cb36176279fd82275fe885cb8753fb18da825c1
Thomas Dilling-Hansen <thomas@dillingdata.se>
2025-06-10 18:54:02 +0200

    Added needed NugetPackages

--- CloudGaminWheelController/obj/Debug/net9.0/CloudGaminWheelController.AssemblyInfo.cs
+++ CloudGaminWheelController/obj/Debug/net9.0/CloudGaminWheelController.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloudGaminWheelController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af2149be4852ff620af14073d7e5c5f66d2fc42a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloudGaminWheelController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloudGaminWheelController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
